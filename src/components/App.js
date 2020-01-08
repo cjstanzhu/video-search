710a1fa9558afefeb161ff92c518fe345ecef2df
cjstanzhu <cjstanzhu@gmail.com>
2020-01-08 16:33:16 -0500

    insert video search code

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,24 @@ import youtube from "../api/youtube"
 
 class App extends React.Component {
     state = {
+        videos: []
+    };
 
+    onTermSubmit = async term => {
+        const response = await youtube.get("/search", {
+            params: {
+                q: term
+            }
+        });
+
+        this.setState({videos: response.data.items});
     };
 
     render() {
         return (
-            <>
-            </>
+            <div className="ui container" style={{marginTop: "10px"}}>
+                <SearchBar onFormSubmit={this.onTermSubmit} />
+            </div>
         );
     };
 };
